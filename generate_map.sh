e7cf57c920030b20a11fe7fd081cb45313e69903
Tyler Landle <tlandle3@gatech.edu>
2021-07-31 21:05:55 +0000

    Updating for Testing data, etc

---
+++ generate_map.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export PYTHONPATH=$PYTHONPATH:$CARLA_ROOT/PythonAPI/carla
+export PYTHONPATH=$PYTHONPATH:$CARLA_ROOT/PythonAPI/carla/dist/carla-0.9.10-py3.7-linux-x86_64.egg
+export PYTHONPATH=$PYTHONPATH:leaderboard
+export PYTHONPATH=$PYTHONPATH:leaderboard/team_code
+export PYTHONPATH=$PYTHONPATH:scenario_runner
+
+python3 generate_map_waypoints.py
---
+++ run_test_autonomous.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source ~/env37/bin/activate
+
+export CARLA_ROOT=/home/ecloud/carla           # change to where you installed CARLA
+export PORT=2000                                                    # change to port that CARLA is running on
+export ROUTES=/home/ecloud/2020_CARLA_challenge/route_aaron_test.xml         # change to desired route
+export TEAM_AGENT=image_agent.py                                    # no need to change
+#export TEAM_AGENT=auto_pilot.py
+export TEAM_CONFIG=~/Downloads/epoch\=24.ckpt                                       # change path to checkpoint
+#export TEAM_CONFIG=sample_data/
+export HAS_DISPLAY=1  
+
+./run_agent.sh
